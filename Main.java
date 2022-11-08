0858216fa62d8803fbab40c5833163b4241d3f60
sasakitoma <t.sasaki.sys22@morijyobi.ac.jp>
2022-11-08 12:22:14 +0900

    髢馴間違い

--- Main.java
+++ Main.java
@@ -3,14 +3,17 @@ public class Main {
 	
 	public static void main(String[] args) {
 	 Scanner scan = new Scanner(System.in);
+	 for(int i=0; i<2;i++){
                 System.out.println("身長");
                 double height = scan.nextDouble();
                 System.out.println("体重");
                 double weight = scan.nextDouble();
 		double height2 = height / 100.0;
-                double BMI = weight / (height2*height2);
+
+                double BMI = weight / Math.pow(height,2);
 
                 System.out.println((double)Math.round(BMI * 100)/100);
+	 }
                 scan.close();
 
 		
